22a847ad81cb57c61ab0846170d6979ff202bc51
Dave Hanley <dave@localhost.(none)>
2013-02-14 17:32:04 -0600

    working on file import e-mails

--- scripts/import.py
+++ scripts/import.py
@@ -10,22 +10,36 @@ def run( str ):
 
 potentials = run( 'find /XRaid/bridge/' )
 
-for file in potentials:
+try:
+ for file in potentials:
    file = file.strip()
-   already = db( db.t_file.f_path == file ).select()
+   fullpath = file
+   file = file.split( '/' )
+   file = file[ -1 ] 
+
+   already = db( db.t_file.f_filename == file ).select()
 
    if len( already ) == 0:
      fn = file.split( '/' )[ -1 ]
      bn_id = fn.split( '_' )[ 0 ]
      
-     id = db.t_file.insert( f_path = file , f_bionimbus_id = bn_id )
-     if len( bn_id.split( '-' ) ) <> 2:
-       print "malformed bionimbus ID:" , bn_id 
+     ids = db( db.t_experiment_unit.f_bionimbus_id == bn_id ).select()
+     if len( ids ) <> 1:
+       #print "malformed bionimbus ID:" , bn_id 
        continue 
+     id = ids[ 0 ] 
+     project = id[ db.t_experiment_unit.f_project ]
+
+     id = db.t_file.insert( f_path = file , f_bionimbus_id = bn_id )
+
      newpath,newname = generate_name( id , bn_id , fn )
-     print file , newpath , newname
+     print "going to import" , file , newpath , newname
      run( "mkdir -p " + newpath )
-     run( "ln " + file + " " + newname )
-     db( db.t_file.id == id ).update( f_newpath = newname )
-     db.commit()
+     run( "ln " + fullpath + " " + newname )
+     db( db.t_file.id == id ).update( f_newpath = newname , f_filename = file )
      print "**** fin!" 
+
+finally:
+  db.rollback()
+
+
