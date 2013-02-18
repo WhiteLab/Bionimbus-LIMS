9cca399e538608380f77cd582cadeb0d9bed1037
Dave Hanley <dave@localhost.(none)>
2013-02-18 16:26:13 -0600

    send import e-mails.

--- scripts/import.py
+++ scripts/import.py
@@ -2,16 +2,17 @@ import sys
 import os
 
 from applications.Bionimbus.modules.file_location import generate_name
+from applications.Bionimbus.modules.mail          import sendMailTo
 
 def run( str ):
   print str
   return os.popen( str ).readlines()
-  
-
+ 
 potentials = run( 'find /XRaid/bridge/' )
 
-try:
- for file in potentials:
+by_project = {}
+
+for file in potentials:
    file = file.strip()
    fullpath = file
    file = file.split( '/' )
@@ -30,6 +31,11 @@ try:
      id = ids[ 0 ] 
      project = id[ db.t_experiment_unit.f_project ]
 
+     if not by_project.has_key( project ):
+       by_project[ project ] = []
+
+     by_project[ project ].append( fn ) 
+
      id = db.t_file.insert( f_path = file , f_bionimbus_id = bn_id )
 
      newpath,newname = generate_name( id , bn_id , fn )
@@ -39,7 +45,9 @@ try:
      db( db.t_file.id == id ).update( f_newpath = newname , f_filename = file )
      print "**** fin!" 
 
-finally:
-  db.rollback()
+for project in by_project.keys():
+  files = by_project[ project ]
+  pname = db.t_project[ project ].f_name
+  sendMailTo( db , 'dhanley@uchicago.edu' , "Files imported to " + pname , "\n".join( files ) , list = 'Import' , project = project )
 
 
