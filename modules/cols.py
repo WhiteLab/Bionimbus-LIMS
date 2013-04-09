5b29a5f89f2aadcc354222a9ef1b4c64ae9048e9
David Hanley <ctchrinthry@gmail.com>
2013-04-09 16:56:18 -0500

    view data by type

---
+++ modules/cols.py
@@ -0,0 +1,63 @@
+
+def rna_cols( db ):
+  eu = db.t_experiment_unit
+  return [ eu.f_name ,
+                       eu.f_sample ,
+                       eu.f_treatment ,
+                       eu.f_prep_preformed_by ,
+                       eu.f_lib_prep_protocol ,
+                       eu.f_barcode ,
+                       eu.f_desired_multiplexing ,
+                       eu.f_read_length ,
+                       eu.f_read_type ,
+                       eu.f_desired_minimum_reads ]
+
+def dna_cols( db ):
+  eu = db.t_experiment_unit
+  return [ eu.f_name ,
+                       eu.f_sample ,
+                       eu.f_treatment ,
+                       eu.f_prep_preformed_by ,
+                       eu.f_lib_prep_protocol ,
+                       eu.f_barcode ,
+                       eu.f_desired_multiplexing ,
+                       eu.f_read_length ,
+                       eu.f_read_type ,
+                       eu.f_desired_minimum_reads ]
+
+def exome_cols( db ):
+  eu = db.t_experiment_unit
+  return  [ eu.f_name ,
+                       eu.f_sample ,
+                       eu.f_treatment ,
+                       eu.f_prep_preformed_by ,
+                       eu.f_lib_prep_protocol ,
+                       eu.f_barcode ,
+                       eu.f_whole_exome_custom_capture ,
+                       eu.f_capture_protocol ,
+                       eu.f_capture_size ,
+                       eu.f_desired_multiplexing ,
+                       eu.f_read_length ,
+                       eu.f_read_type ,
+                       eu.f_desired_minimum_reads ]
+
+def chipseq_cols( db ):
+  eu = db.t_experiment_unit
+  return [ eu.f_name ,
+                       eu.f_strain ,
+                       eu.f_tissue ,
+                       eu.f_source ,
+                       eu.f_replicate ,
+                       eu.f_agent ,
+                       eu.f_target_symbol ,
+                       eu.f_target_ID ,
+                       eu.f_fb_wb_ID ,
+                       eu.f_treatment ,
+                       eu.f_prep_preformed_by ,
+                       eu.f_lib_prep_protocol ,
+                       eu.f_barcode ,
+                       eu.f_desired_multiplexing ,
+                       eu.f_read_length ,
+                       eu.f_read_type ,
+                       eu.f_desired_minimum_reads ]
+
