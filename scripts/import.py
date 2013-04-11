ad8d8ac504f9d84f60b6bc44a0f097247809f929
David Hanley <ctchrinthry@gmail.com>
2013-04-11 15:06:24 -0500

    add metadata to file import
    break up import based in types

--- scripts/import.py
+++ scripts/import.py
@@ -4,6 +4,11 @@ import os
 from applications.Bionimbus.modules.file_location import generate_name
 from applications.Bionimbus.modules.mail          import sendMailTo
 
+from applications.Bionimbus.modules.cols import *
+
+def flatten( x ):
+  return str(x) 
+
 def run( str ):
   print str
   return os.popen( str ).readlines()
@@ -20,7 +25,11 @@ def file_len( fname ):
       pass
     return i + 1
 
-potentials = run( 'find /XRaid/bridge/' )
+path = '/XRaid/bridge/'
+test_path = '/home/dave/tmp/'
+#path = test_path
+
+potentials = run( 'find ' + path  )
 
 by_project = {}
 
@@ -36,19 +45,22 @@ for file in potentials:
      fn = file.split( '/' )[ -1 ]
      bn_id = fn.split( '_' )[ 0 ]
      
-     ids = db( db.t_experiment_unit.f_bionimbus_id == bn_id ).select()
-     if len( ids ) <> 1:
+     row = db( db.t_experiment_unit.f_bionimbus_id == bn_id ).select()
+     if len( row ) <> 1:
        #print "malformed bionimbus ID:" , bn_id 
        continue 
-     id = ids[ 0 ] 
-     project = id[ db.t_experiment_unit.f_project ]
+     row = row[ 0 ] 
+     type = row[ db.t_experiment_unit.f_type ]
+     if type == None:
+       type = ''
+     project = str( row[ db.t_experiment_unit.f_project ] ) + ',' + type
 
      if not by_project.has_key( project ):
        by_project[ project ] = []
 
-     by_project[ project ].append( ( fullpath , fn ) ) 
-
      id = db.t_file.insert( f_path = file , f_bionimbus_id = bn_id )
+
+     by_project[ project ].append( file )
 
      newpath,newname = generate_name( id , bn_id , fn )
      print "going to import" , file , newpath , newname
@@ -57,14 +69,65 @@ for file in potentials:
      db( db.t_file.id == id ).update( f_newpath = newname , f_filename = file )
      print "**** fin!" 
 
+
 for project in by_project.keys():
-  files2 = by_project[ project ]
-  files = []
-  for file,fn in files2:
-    f = "%s ( %d reads )" % ( fn , file_len( file ) / 4  )
-    files.append( f ) 
+  paths = by_project[ project ]
+  project,title = project.split( ',' )
+
+  content = ''
+  print "type =",title 
+
+  res = []
+  if title == 'RNAseq':
+    res = rna_cols(db)
+  elif title == 'DNAseq':
+    res = dna_cols(db)
+  elif title == 'Exomes':
+    res = exome_cols(db)
+  elif title == 'ChiPseq':
+    res = chipseq_cols(db)
+  else:
+    print "****unmatched type" , title
+    res = [
+          db.t_experiment_unit.f_bionimbus_id
+        , db.t_experiment_unit.f_name
+        , db.t_experiment_unit.f_project
+        , db.t_experiment_unit.f_subproject
+        , db.t_experiment_unit.f_agent
+        , db.t_experiment_unit.f_organism
+        , db.t_experiment_unit.f_is_public
+    ] 
+  res = [ db.t_file.f_path ] + res
+
+  print paths 
+  for fpath in paths:
+    row = db( ( db.t_experiment_unit.f_bionimbus_id == db.t_file.f_bionimbus_id ) & ( db.t_file.f_path == fpath ) ).select()
+    row = row[ 0 ] 
+    if content == '':
+      content = '<table>'
+      content += '<tr>'
+      for r in res:
+        content += '<td>' + r.label + '</td>'
+      content += '<tr>'
+    
+    content += '<tr>'
+    for r in res:
+      content += '<td>' + flatten( row[ r ] ) + '<td>'
+    content += '</tr>'
+
+    #f = "%s ( %d reads )" % ( fn , file_len( file ) / 4  )
+    # print f 
+
+  content += '</table>'
 
   pname = db.t_project[ project ].f_name
-  sendMailTo( db , 'dhanley@uchicago.edu' , "Files imported to " + pname , "\n".join( files ) , list = 'Import' , project = project )
+  if path == test_path:
+    print '--------'
+    print
+    print content
+    print
+  else:
+    sendMailTo( db , 'dhanley@uchicago.edu' , "Files imported to " + pname , content , list = 'Import' , project = project )
 
+db.rollback()
 
