d7b12ca69a576ca6b0f4fa7417fa1890ee40e1d8
David Hanley <ctchrinthry@gmail.com>
2013-08-13 16:16:56 -0500

    reflect docmentation

--- models/menu.py
+++ models/menu.py
@@ -36,10 +36,12 @@ response.menu = [
 (T('Key Generation'),URL('keygen','keygen_spreadsheet')==URL(),URL('keygen','keygen_spreadsheet'),[]),
 ]
 
-u = A('User Documentation',_href='https://docs.google.com/document/d/17BrXs28Gc1nMmRo94NAajwf3Cu8gAyuOi5YVIhZ0FuA/edit?usp=sharing',_target='docs')
+u = A('User Documentation',_href='https://docs.google.com/document/d/17BrXs28Gc1nMmRo94NAajwf3Cu8gAyuOi5YVIhZ0FuA/view?usp=sharing',_target='docs')
+u2 = A('Quick start',_href='https://docs.google.com/document/d/1RPb8UEQsDgsZmuDCW_rTEMHtAgGCDnNYTm7BaRsmgAo/view?usp=sharing',_target='docs')
 
 response.menu.append( ( 'Docs' , True , None , [
-  (T('Documentation'),False,u,[])
+  (T('Documentation'),False,u,[]),
+  (T('Quick Start'),False,u2,[])
 ] ) )
 
 
