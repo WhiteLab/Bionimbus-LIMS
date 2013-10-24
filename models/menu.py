2e9ac80284c2e51b5c0f54703f4820ff6de3d982
Dave Hanley <ctchrinthry@gmail.com>
2013-10-24 17:21:33 -0500

    doc menu

--- models/menu.py
+++ models/menu.py
@@ -42,12 +42,10 @@ response.menu = [
 (T('Key Generation'),URL('keygen','keygen_spreadsheet')==URL(),URL('keygen','keygen_spreadsheet'),[]),
 ]
 
-u = A('User Documentation',_href='/Bionimbus/static/bd/Bionimbusdocumentation.html',_target='docs')
-u2 = A('Quick start',_href='https://docs.google.com/document/d/1RPb8UEQsDgsZmuDCW_rTEMHtAgGCDnNYTm7BaRsmgAo/view?usp=sharing',_target='docs')
+u = A('User Documentation',_href='https://bc.bionimbus.org/LIMS/static/bd/Bionimbusdocumentation.html',_target='docs')
 
 response.menu.append( ( 'Docs' , True , None , [
-  (T('Documentation'),False,u,[]),
-  (T('Quick Start'),False,u2,[])
+  (T('Documentation'),False,u,[])
 ] ) )
 
 
