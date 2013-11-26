a554c03a67e390fc6336dcfd80d3cec077a02488
Dave Hanley <ctchrinthry@gmail.com>
2013-11-26 14:29:26 -0600

    import code

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,3 +1,3 @@
 cd ../../..
 python web2py.py -S Bionimbus -M -R applications/Bionimbus/scripts/import.py
-#python web2py.py -S Bionimbus -M -R applications/Bionimbus/scripts/fs.py
+python web2py.py -S Bionimbus -M -R applications/Bionimbus/scripts/fs.py
